3b386c1847337b78dacab9dfe4a9074eb03e615f
Tu Nombre <a22300241@ceti.mx>
2025-11-08 17:31:37 -0600

    Add tab1, tab2, and tab3 components with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,17 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'tab1',
+    loadComponent: () => import('./pages/tab1/tab1.page').then( m => m.Tab1Page)
   },
+  {
+    path: 'tab2',
+    loadComponent: () => import('./pages/tab2/tab2.page').then( m => m.Tab2Page)
+  },
+  {
+    path: 'tab3',
+    loadComponent: () => import('./pages/tab3/tab3.page').then( m => m.Tab3Page)
+  },
+
 ];
