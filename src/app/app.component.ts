8a8f5e6c8b3a10d676566ecb24c9fc4f6fa37604
Tu Nombre <a22300241@ceti.mx>
2025-11-13 15:26:28 -0600

    poner navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonTabButton, IonLabel, IonTabBar, IonTabs } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonTabs, IonTabBar, IonLabel, IonTabButton, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
